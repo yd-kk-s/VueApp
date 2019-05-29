7c8d264c1fbc76e24ecfd3844a869e1980f991a5
山田加奈子 <oorer.00@gmail.com>
2019-05-29 16:24:43 +0900

    Created controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,9 @@
+class MessagesController < ApplicationController
+  def index
+    messages = Message.all
+    respond_to do |format|
+      format.html
+      format.json { render json: messages }
+    end
+  end
+end
