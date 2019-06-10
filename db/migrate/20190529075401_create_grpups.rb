d21fa22d1a9cd808b01317cc638b87513f9c5b6b
山田加奈子 <oorer.00@gmail.com>
2019-06-10 13:02:38 +0900

    Migration file creation and editing
    Group model created
    Deleted the unnecessary model
    Added gruop_id column to messages model

--- db/migrate/20190529075401_create_grpups.rb
+++
@@ -1,9 +0,0 @@
-class CreateGrpups < ActiveRecord::Migration[5.2]
-  def change
-    create_table :grpups do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_075401) do
+ActiveRecord::Schema.define(version: 2019_06_04_064515) do
 
-  create_table "grpups", force: :cascade do |t|
+  create_table "groups", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_05_29_075401) do
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "group_id"
   end
 
 end
